6f1e3b244b77056a17527ed73de1acf7602fb374
Liang Zhang <titanzhang@gmail.com>
2017-10-05 09:57:51 -0700

    change dev domain name

--- config/site.js
+++ config/site.js
@@ -1,7 +1,7 @@
 exports.config = {
 	title: 'AlgorithmHub',
-	jspool: 'http://static.dev.algohub.com:8301/' + load('version').version + '/js',
-	csspool: 'http://static.dev.algohub.com:8301/' + load('version').version + '/css',
-	articlepool: 'http://article.dev.algohub.me:4000/algo',
+	jspool: 'http://static.dev.algohub.me:8301/' + load('version').version + '/js',
+	csspool: 'http://static.dev.algohub.me:8301/' + load('version').version + '/css',
+	articlepool: 'http://www.dev.algohub.me:4000/algo',
 	mgmtpath: '/'
 };
