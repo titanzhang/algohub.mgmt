69e2d9e0ca01e67d9338f7de0e6f868e8f1fa110
Liang Zhang <titanzhang@gmail.com>
2017-10-04 22:31:28 -0700

    Add static resource version

--- config/site.js
+++ config/site.js
@@ -1,7 +1,7 @@
 exports.config = {
 	title: 'AlgorithmHub',
-	jspool: 'http://static.dev.algohub.com:8301/js',
-	csspool: 'http://static.dev.algohub.com:8301/css',
+	jspool: 'http://static.dev.algohub.com:8301/' + load('version').version + '/js',
+	csspool: 'http://static.dev.algohub.com:8301/' + load('version').version + '/css',
 	articlepool: 'http://article.dev.algohub.me:4000/algo',
 	mgmtpath: '/'
 };
