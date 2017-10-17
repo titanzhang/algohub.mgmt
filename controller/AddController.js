64a9dea20da600edddf28ebfb605bc8c57be55d9
Liang Zhang <titanzhang@gmail.com>
2017-10-17 10:58:12 -0700

    Refactor to ES6

--- controller/AddController.js
+++ controller/AddController.js
@@ -1,10 +1,10 @@
 module.exports = function(request, response) {
-	const controller = new AddController();
-	controller.run()
-		.then( (result) => {
+	(async () => {
+		try {
+			const controller = new AddController();
+			const result = await controller.run();
 			response.render(result.template, result.result);
-		})
-		.catch( (error) => {
+		} catch(error) {
 			load('common.Utils').log(request.originalUrl, error.message);
 			response.render('page/error', {
 				site: loadConfig('site').config,
@@ -12,27 +12,29 @@ module.exports = function(request, response) {
 				errorTitle: 'This page is temporary unavailable',
 				errorMessage: 'The page you visited may be broken, or under maintanence.'
 			});
-		});
-};
-
-var AddController = function() {
-};
-
-AddController.prototype.run = function() {
-	const siteConfig = loadConfig('site').config;
-	const pageTitle = siteConfig.title + ' - New';
-	const steps = load('common.BizShared').buildSteps();
-	const allTags = load('common.BizShared').getAlgoTags();
-
-	return Promise.resolve({
-		template: 'page/name',
-		result: {
-			site: siteConfig,
-			customTitle: pageTitle,
-			steps: steps,
-			isNew: true,
-			tagList: [],
-			allTags: allTags
 		}
-	});
+	})();
+};
+
+class AddController {
+
+	async run() {
+		const siteConfig = loadConfig('site').config;
+		const pageTitle = siteConfig.title + ' - New';
+		const steps = load('common.BizShared').buildSteps();
+		const allTags = load('common.BizShared').getAlgoTags();
+
+		return {
+			template: 'page/name',
+			result: {
+				site: siteConfig,
+				customTitle: pageTitle,
+				steps: steps,
+				isNew: true,
+				tagList: [],
+				allTags: allTags
+			}
+		};
+	}
+
 }
